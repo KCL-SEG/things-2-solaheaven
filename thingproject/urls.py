19624ec19af5ecce2b4b780635bede86166e9aab
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-16 00:23:44 +0100

    Solved

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -21,5 +21,4 @@ from things import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('thing_form/', views.thing_form, name ='thing_form'),
-]
+    ]
